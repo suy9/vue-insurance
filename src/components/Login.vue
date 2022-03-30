7c4169e44ca03591adf19332b382d7d37e856411
suy9 <suy9@foxmail.com>
2022-03-30 14:44:07 +0800

    master

---
+++ src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    登陆组件
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Login'
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import Login from '@/components/Login'
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  { pathh: '/login', component: Login }
 ]
 
 const router = createRouter({
